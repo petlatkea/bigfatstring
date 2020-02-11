e8416781c38800d460b95ce9e8a85a5f18dcbf68
petlatkea <petl@kea.dk>
2020-02-11 23:14:59 +0100

    basic scripting

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("Started");
+  document.querySelector("button#generate").addEventListener("click", generateOutput);
+}
+
+function generateOutput( event ) {
+  console.log("generate output");
+  event.preventDefault();
+
+  const choice = document.querySelector("#choice").value;
+  console.log(`Choice: ${choice}`);
+
+  const input = document.querySelector("#input").value;
+  let output = "";
+
+  switch( choice ) {
+    case "capitalize":
+      break;
+    case "firstname":
+      break;
+    case "firstlength":
+      break;
+    case "middlename":
+      break;
+    case "filecheck":
+      break;
+    case "password":
+      break;
+    case "3capitalize":
+      break;
+    case "hyphen":
+      break;
+  }
+  // TEST Only - REMOVE!!!
+  output = input;
+
+  document.querySelector("#output").textContent = output;
+
+}
